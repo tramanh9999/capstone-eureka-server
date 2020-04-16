7455dd4f31aca4f85aa357d0cf8249a8fab1d094
tramanh9999 <43420892+tramanh9999@users.noreply.github.com>
2020-04-16 15:32:33 +0700

    create EUREKA

---
+++ src/test/java/com/storyart/eurakaserver/EurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storyart.eurakaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
